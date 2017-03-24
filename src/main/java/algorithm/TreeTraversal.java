7265116db1569fd6f03861187fcfd4bfdaad96be
Kevin Liu <kevin@ticketfly.com>
2017-03-24 10:03:33 -0700

    Added BST

--- src/main/java/algorithm/TreeTraversal.java
+++ src/main/java/algorithm/TreeTraversal.java
@@ -2,15 +2,16 @@ package algorithm;
 
 import datastructure.BTreeNode;
 
+
 class TreeTraversal {
 
   public static void main (String[] args) {
-    BTreeNode node1 = new BTreeNode(1);
-    BTreeNode node2 = new BTreeNode(2);
-    BTreeNode node3 = new BTreeNode(3);
-    BTreeNode node4 = new BTreeNode(4);
-    BTreeNode node5 = new BTreeNode(5);
-    BTreeNode node6 = new BTreeNode(6);
+      BTreeNode<Integer> node1 = new BTreeNode<>(1);
+      BTreeNode<Integer> node2 = new BTreeNode<>(2);
+      BTreeNode<Integer> node3 = new BTreeNode<>(3);
+      BTreeNode<Integer> node4 = new BTreeNode<>(4);
+      BTreeNode<Integer> node5 = new BTreeNode<>(5);
+      BTreeNode<Integer> node6 = new BTreeNode<>(6);
 
     node1.left = node2;
     node1.right = node3;
@@ -18,8 +19,17 @@ class TreeTraversal {
     node2.right = node5;
     node3.left = node6;
 
-    BTreePrinter p = new BTreePrinter();
-    p.printNode(node1);
+
+      BTreePrinter.printNode(node1);
+
+      System.out.println("\nPRE ORDER: ");
+      preOrderTraversal(node1);
+
+      System.out.println("\nPOST ORDER: ");
+      postOrderTraversal(node1);
+
+      System.out.println("\nIN ORDER: ");
+      inOrderTraversal(node1);
   }
 
   // left to right
@@ -29,11 +39,23 @@ class TreeTraversal {
 
   // up to down
   private static void preOrderTraversal(BTreeNode node) {
-
+      if (node == null) return;
+      System.out.print(node.data + ", ");
+      preOrderTraversal(node.left);
+      preOrderTraversal(node.right);
   }
 
   // down to up
   private static void postOrderTraversal(BTreeNode node) {
+      if (node.left == null && node.right == null) {
+          System.out.print(node.data + ", ");
+          return;
+      }
+
+      postOrderTraversal(node.left);
+
+      postOrderTraversal(node.right);
+
 
   }
 }
---
+++ src/main/java/datastructure/BST.java
@@ -0,0 +1,46 @@
+package datastructure;
+
+
+class BST<T> {
+
+    public BTreeNode<T> root;
+
+    public BST() {
+        root = null;
+    }
+
+    public void insert(T value) {
+
+    }
+
+    public T getMaxValue() {
+
+    }
+
+    public T getMinValue() {
+
+    }
+
+    public int getMinHeight() {
+
+    }
+
+    public int getMaxHeight() {
+
+    }
+
+    public void inOrderTraversal() {
+
+    }
+
+    public void preOrderTraversal() {
+
+    }
+
+    public void postOrderTraversal() {
+
+    }
+
+
+
+}
